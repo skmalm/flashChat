09aee9baa18f70b658e7760f7404575dad5f66e0
Sebastian Malm <skmalm@gmail.com>
2020-03-08 18:44:27 -0400

    Initial Commit

---
+++ flashChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flashChat
+//
+//  Created by Sebastian Malm on 3/8/20.
+//  Copyright © 2020 SebastianMalm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
