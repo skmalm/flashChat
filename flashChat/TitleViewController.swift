9ae0ea0b72f3ab9fe42dca79cadbefbcdf074ee5
Sebastian Malm <skmalm@gmail.com>
2020-03-08 19:02:27 -0400

    feat: Add titleViewController with title

---
+++ flashChat/TitleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TitleViewController.swift
+//  flashChat
+//
+//  Created by Sebastian Malm on 3/8/20.
+//  Copyright © 2020 SebastianMalm. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    
+
+}
