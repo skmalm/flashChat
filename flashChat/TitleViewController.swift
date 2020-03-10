7027d476e5af8566ee6821cb547f1e6250be3f73
Sebastian Malm <skmalm@gmail.com>
2020-03-10 12:00:10 -0400

    feat: Add animation to title scene text

--- flashChat/TitleViewController.swift
+++ flashChat/TitleViewController.swift
@@ -13,10 +13,35 @@ class TitleViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
+        appNameLabel.text = "⚡️"
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let titleString = "⚡️FlashChat"
+        var indexTracker = 0
+        Timer.scheduledTimer(withTimeInterval: Constants.titleAnimationInterval, repeats: true) { timer in
+            if indexTracker >= titleString.count {
+                timer.invalidate()
+            } else {
+                let index = titleString.index(titleString.startIndex, offsetBy: indexTracker)
+                let outputString = String(titleString[titleString.startIndex...index])
+                let spaces = String(repeating: " ", count: titleString.count - outputString.count)
+                self.appNameLabel.text = outputString + spaces
+                indexTracker += 1
+            }
+        }
+    }
+    
+    @IBOutlet weak var appNameLabel: UILabel!
+}
+
+extension TitleViewController {
+    struct Constants {
+        static let titleAnimationInterval: TimeInterval = 0.08
+    }
 }
