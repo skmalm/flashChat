8c71005e7017c692d12059cec3642a79fdd2c0a9
Sebastian Malm <skmalm@gmail.com>
2020-03-22 14:15:46 -0400

    feat: Add some IQKeyboardManager customizations
    Unnecessary keyboard toolbar is now gone.
    Keyboard now hides when user taps elsewhere.

--- flashChat/App & Scene Delegates/AppDelegate.swift
+++ flashChat/App & Scene Delegates/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let db = Firestore.firestore()
         print(db)
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
 
