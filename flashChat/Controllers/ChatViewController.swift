d09ce3b4d09c32ef573fd4f06dde674f01c6dba1
Sebastian Malm <skmalm@gmail.com>
2020-03-22 12:09:16 -0400

    feat: CVC table view scrolls to bottom
    Scrolling to bottom happens upon every Firestore data read

--- flashChat/Controllers/ChatViewController.swift
+++ flashChat/Controllers/ChatViewController.swift
@@ -41,6 +41,9 @@ class ChatViewController: UIViewController {
                 self.messages = allMessages
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
+                    if let bottomRow = self.tableView.indexPathsForVisibleRows?.last {
+                        self.tableView.scrollToRow(at: bottomRow, at: .bottom, animated: true)
+                    }
                 }
             }
     }
@@ -118,12 +121,16 @@ extension ChatViewController: UITableViewDataSource {
         // if message is from logged in user
         if message.sender == user?.email {
             cell.messageLabel.text = message.body
+            cell.meAvatar.isHidden = false
             cell.youAvatar.isHidden = true
             cell.messageView.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
             cell.messageLabel.textColor = UIColor(named: K.BrandColors.purple)
         } else { // if message is not from logged in user
             cell.messageLabel.text = "\(message.sender): \(message.body)"
             cell.meAvatar.isHidden = true
+            cell.youAvatar.isHidden = false
+            cell.messageView.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.messageLabel.textColor = UIColor(named: K.BrandColors.lightPurple)
         }
         return cell
     }
